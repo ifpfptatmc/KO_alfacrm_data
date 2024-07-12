b32c29cc88e8d155bb94015d1845151e76b757d4
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-12 18:13:17 +0400

    Update fetch_leads_status.py

--- fetch_leads_status.py
+++ fetch_leads_status.py
@@ -1,8 +1,9 @@
 import requests
 import csv
 import os
+from datetime import datetime
 
-def fetch_all_leads_status():
+def fetch_leads_status():
     email = os.getenv('ALPHA_CRM_EMAIL')
     api_key = os.getenv('ALPHA_CRM_API_KEY')
     hostname = os.getenv('ALPHA_CRM_HOSTNAME')
@@ -15,39 +16,37 @@ def fetch_all_leads_status():
     if response.status_code == 200:
         token = response.json().get('token')
         print('Токен:', token)
-        
-        # Запрос логов для получения статусов лидов
+
+        # Запрос логов для получения лидов
         logs_url = f'https://{hostname}/v2api/log/index'
         headers = {'X-ALFACRM-TOKEN': token, 'Accept': 'application/json', 'Content-Type': 'application/json'}
 
+        lead_ids = set()
         page = 0
         per_page = 100
-        lead_statuses = []
 
         while True:
             logs_params = {
                 'filters': {
-                    'entity': 'customer',
-                    'fields_new': {
-                        'lead_status_id': {'$exists': True}
-                    }
+                    'entity': 'customer'
                 },
                 'page': page,
                 'per-page': per_page
             }
-            
+
             response = requests.post(logs_url, headers=headers, json=logs_params)
 
             if response.status_code == 200:
                 logs = response.json().get('items', [])
                 if not logs:
                     break
+
                 for log in logs:
-                    lead_id = log['entity_id']
-                    status_id = log['fields_new'].get('lead_status_id')
-                    if status_id:
-                        lead_statuses.append({'Lead ID': lead_id, 'Status ID': status_id})
-                
+                    if 'fields_new' in log:
+                        for field in log['fields_new']:
+                            if 'lead_status_id' in field:
+                                lead_ids.add((log['entity_id'], field['lead_status_id']))
+
                 page += 1
             else:
                 print('Ошибка получения логов:', response.text)
@@ -58,12 +57,12 @@ def fetch_all_leads_status():
             fieldnames = ['Lead ID', 'Status ID']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            for lead_status in lead_statuses:
-                writer.writerow(lead_status)
-
-        print('Список статусов лидов сохранен в leads_status.csv')
+            for lead_id, status_id in lead_ids:
+                writer.writerow({'Lead ID': lead_id, 'Status ID': status_id})
+        
+        print('Список ID лидов сохранен в leads_status.csv')
     else:
         print('Ошибка авторизации:', response.text)
 
 if __name__ == "__main__":
-    fetch_all_leads_status()
+    fetch_leads_status()
